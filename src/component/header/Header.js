28c853750fa891d0490a03cefa925cb0cb65de14
CaterJo <kin7729@naver.com>
2019-03-24 17:10:59 +0900

    header

---
+++ src/component/header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <div className = "container">
+          <div className = "menu">
+            헤더 내용
+          </div>
+          <div className = "icon">
+            아이콘
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Header;
