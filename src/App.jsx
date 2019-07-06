ff2677921c7a7d5321c9ab539713224c800d1737
SeonHyungJo <seonhyung.jo@gmail.com>
2019-07-07 01:31:20 +0900

    :foggy: SeonHyungJo / Add Toast ui Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import Map from './component/map/map'
+import 'tui-grid/dist/tui-grid.css'
+import Grid from '@toast-ui/react-grid'
 
 class App extends Component {
   render () {
+    const data = [
+      { id: 1, name: 'Editor' },
+      { id: 2, name: 'Grid' },
+      { id: 3, name: 'Chart' }
+    ]
+
+    const columns = [
+      { name: 'id', title: 'ID' },
+      { name: 'name', title: 'Name' }
+    ]
     return (
-      <Map ncpClientId={process.env.REACT_APP_SERVICEKEY}/>
+      <>
+        <Map ncpClientId={process.env.REACT_APP_SERVICEKEY}/>
+        <Grid
+          data={data}
+          columns={columns}
+          rowHeight={25}
+          bodyHeight={100}
+          virtualScrolling={true}
+          heightResizable={false}
+          rowHeaders={['rowNum']}
+        />
+      </>
     )
   }
 }
